f80f82189797d1892cf7c9a943d3da41cab5807d
Balu-Rama-Chandra <41617154+Balurc@users.noreply.github.com>
2019-11-27 16:05:54 +0700

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,6 +8,7 @@ from starlette.applications import Starlette
 from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
+from flask import Flask, request, jsonify
 
 export_file_url = 'https://drive.google.com/uc?export=download&id=1o3wmeFwBXp88ZBtfUYK8nAyVOAWqDF5c'
 export_file_name = 'resnet50(91).pkl'
@@ -70,7 +71,7 @@ async def analyze(request):
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)
     probs_list = prediction[2].numpy()
-    return JSONResponse({
+    return jsonify({
         'category': classes[prediction[1].item()],
         'probs': {c: round(float(probs_list[i]), 5) for (i, c) in enumerate(classes)}
     })
